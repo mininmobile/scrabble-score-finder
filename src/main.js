3c81d53837df3562e9f9ae01a90d6a65ccaaa11f
MininMobile <notminin@pm.me>
2018-12-03 22:51:59 +0000

    add updateScore

--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
 let letters = document.getElementById("letters");
 let score = document.getElementById("score");
 
+let dictionary = {
+	"a": 1,
+	"b": 3,
+	"c": 3,
+	"d": 2,
+	"e": 1,
+	"f": 4,
+	"g": 2,
+	"h": 4,
+	"i": 1,
+	"j": 8,
+	"k": 5,
+	"l": 1,
+	"m": 3,
+	"n": 1,
+	"o": 1,
+	"p": 3,
+	"q": 10,
+	"r": 1,
+	"s": 1,
+	"t": 1,
+	"u": 1,
+	"v": 4,
+	"w": 4,
+	"x": 8,
+	"y": 4,
+	"z": 10
+}
+
 document.addEventListener("keydown", (e) => {
 	if (e.key == "Backspace") {
 		document.querySelector("#letters *:last-child").remove();
@@ -13,3 +42,13 @@ document.addEventListener("keydown", (e) => {
 
 	updateScore();
 });
+
+function updateScore() {
+	let result = 0;
+
+	letters.childNodes.forEach((l) => {
+		result += dictionary[l.textContent] ? dictionary[l.textContent] : 0;
+	});
+
+	score.innerText = result;
+}
